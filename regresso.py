93c9e168633a62aaa304ce4bfeb7c80df9f043d8
David Maël Cormier <david.mael.cormier@gmail.com>
2020-09-29 12:17:56 -0400

    first commit

---
+++ regresso.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#fit a curve to input data
+
+#input
+
+#elaborate equation terms: A matrix, x and b vectors
+
+#"solve equation"
+
+#output graph
